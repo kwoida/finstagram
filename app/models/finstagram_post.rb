d6b91cc1f2fdbee8cac6d9f22a25a662cc6827b1
kwoida <woidakelly@gmail.com>
2021-02-02 04:36:53 +0000

    Active Record

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -1,3 +1,29 @@
 class FinstagramPost < ActiveRecord::Base
 
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  validates_presence_of :user
+
+  def humanized_time_ago
+    time_ago_in_minutes = Time.now - self.created_at
+    time_ago_in_days = time_ago_in_minutes / 1440
+
+    if time_ago_in_days >= 1
+      "#{(time_ago_in_days / 24).to_i} days ago"
+    else
+      "#{time_ago_in_days.to_i} days ago"
+    end
+  end
+
+  def like_count
+    self.likes.size
+  end
+
+  def comment_count
+    self.comments.size
+  end
+
 end
+
