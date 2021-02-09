0a0cb06a1630edbf1062d37560467479d409c7a9
kwoida <woidakelly@gmail.com>
2021-02-09 04:21:13 +0000

    add get,post login routes; add current user helper method

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,31 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
+end
+
+get '/login' do    # when a GET request comes into /login
+  erb(:login)      # render app/views/login.erb
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
 end
 
 get '/signup' do     # if a user navigates to the path "/signup",
@@ -17,8 +42,13 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
